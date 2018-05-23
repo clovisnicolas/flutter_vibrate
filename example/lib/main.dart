b6bf7f28d61e4a4b034734582242136d3d505e58
Felix Emmenlauer <f.emmenlauer@gmail.com>
2018-05-23 21:08:51 +0200

    * Add custom duration to example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:vibrate/vibrate.dart';
 
@@ -9,13 +11,17 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   bool _canVibrate = true;
-  final Iterable<Duration> pauses = [
+
+  final Iterable<Duration> _pauses = const [
     const Duration(milliseconds: 500),
     const Duration(milliseconds: 1000),
     const Duration(milliseconds: 500),
   ];
+
+  bool _supportsCustomDuration = false;
+
+  Duration _duration = const Duration(milliseconds: 1000);
 
   @override
   initState() {
@@ -23,9 +29,12 @@ class _MyAppState extends State<MyApp> {
     init();
   }
 
-  init() async{
+  init() async {
     bool canVibrate = await Vibrate.canVibrate;
-    setState((){_canVibrate = canVibrate;});
+    setState(() {
+      _canVibrate = canVibrate;
+      _supportsCustomDuration = Platform.operatingSystem == "android";
+    });
   }
 
   @override
@@ -34,17 +43,42 @@ class _MyAppState extends State<MyApp> {
       home: new Scaffold(
         appBar: new AppBar(title: new Text('Plugin example app')),
         body: new Center(
-          child: new Column(
-            children: <Widget>[
-              new ListTile(
+          child: new Column(children: <Widget>[
+            new ListTile(
                 leading: new Icon(Icons.vibration, color: Colors.teal),
-                onTap: _canVibrate ?(){Vibrate.vibrate();} : null,
-                title: new Text("Vibrate")),
-              new ListTile(
-                  leading: new Icon(Icons.vibration, color: Colors.blue),
-                  onTap: _canVibrate ?(){Vibrate.vibrateWithPauses(pauses);} : null,
-                  title: new Text("Vibrate with pauses")),
-            ]),
+                onTap: _canVibrate
+                    ? () {
+                        if (_supportsCustomDuration) {
+                          Vibrate.vibrate(duration: _duration);
+                        } else {
+                          Vibrate.vibrate();
+                        }
+                      }
+                    : null,
+                title: new Text("Vibrate"),
+                subtitle: _supportsCustomDuration
+                    ? new Row(children: <Widget>[
+                        new Slider(
+                            min: 500.0,
+                            max: 5000.0,
+                            value: _duration.inMilliseconds.roundToDouble(),
+                            onChanged: (double value) {
+                              setState(() {
+                                _duration = new Duration(milliseconds: value.round());
+                              });
+                            }),
+                        new Text("${_duration.inMilliseconds} ms"),
+                      ])
+                    : null),
+            new ListTile(
+                leading: new Icon(Icons.vibration, color: Colors.blue),
+                onTap: _canVibrate
+                    ? () {
+                        Vibrate.vibrateWithPauses(_pauses);
+                      }
+                    : null,
+                title: new Text("Vibrate with pauses")),
+          ]),
         ),
       ),
     );
