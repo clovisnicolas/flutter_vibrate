cd6d62854f1d59946ef4d711cb413d01b1d80a04
nohli <43643339+nohli@users.noreply.github.com>
2021-09-17 23:07:38 +0200

    Auto-format

--- android/src/main/java/flutter/plugins/vibrate/VibratePlugin.java
+++ android/src/main/java/flutter/plugins/vibrate/VibratePlugin.java
@@ -17,8 +17,8 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
  */
 public class VibratePlugin implements MethodCallHandler {
 
-  private VibratePlugin(Registrar registrar){
-    this._vibrator = (Vibrator)registrar.context().getSystemService(Context.VIBRATOR_SERVICE);
+  private VibratePlugin(Registrar registrar) {
+    this._vibrator = (Vibrator) registrar.context().getSystemService(Context.VIBRATOR_SERVICE);
   }
 
   private Vibrator _vibrator;
@@ -32,106 +32,99 @@ public class VibratePlugin implements MethodCallHandler {
   @SuppressWarnings("deprecation")
   public void onMethodCall(MethodCall call, Result result) {
     if (call.method.equals("vibrate")) {
-      if(_vibrator.hasVibrator()){
+      if (_vibrator.hasVibrator()) {
         int duration = call.argument("duration");
         if (Build.VERSION.SDK_INT >= 26) {
-            _vibrator.vibrate(VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE));
+          _vibrator.vibrate(VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE));
         } else {
-            _vibrator.vibrate(duration);
+          _vibrator.vibrate(duration);
         }
       }
       result.success(null);
-    }
-    else if(call.method.equals("canVibrate")){
+    } else if (call.method.equals("canVibrate")) {
       result.success(_vibrator.hasVibrator());
-    } //Feedback
-    else if(call.method.equals("impact")){
-      if(_vibrator.hasVibrator()){
+    } // Feedback
+    else if (call.method.equals("impact")) {
+      if (_vibrator.hasVibrator()) {
         if (Build.VERSION.SDK_INT >= 26) {
-            _vibrator.vibrate(VibrationEffect.createOneShot(HapticFeedbackConstants.VIRTUAL_KEY, VibrationEffect.DEFAULT_AMPLITUDE));
+          _vibrator.vibrate(
+              VibrationEffect.createOneShot(HapticFeedbackConstants.VIRTUAL_KEY, VibrationEffect.DEFAULT_AMPLITUDE));
         } else {
-            _vibrator.vibrate(HapticFeedbackConstants.VIRTUAL_KEY);
+          _vibrator.vibrate(HapticFeedbackConstants.VIRTUAL_KEY);
         }
       }
       result.success(null);
-    }
-    else if(call.method.equals("selection")){
-      if(_vibrator.hasVibrator()){
+    } else if (call.method.equals("selection")) {
+      if (_vibrator.hasVibrator()) {
         if (Build.VERSION.SDK_INT >= 26) {
-           _vibrator.vibrate(VibrationEffect.createOneShot(HapticFeedbackConstants.KEYBOARD_TAP, VibrationEffect.DEFAULT_AMPLITUDE));
+          _vibrator.vibrate(
+              VibrationEffect.createOneShot(HapticFeedbackConstants.KEYBOARD_TAP, VibrationEffect.DEFAULT_AMPLITUDE));
         } else {
-            _vibrator.vibrate(HapticFeedbackConstants.KEYBOARD_TAP);
+          _vibrator.vibrate(HapticFeedbackConstants.KEYBOARD_TAP);
         }
       }
       result.success(null);
-    }
-    else if(call.method.equals("success")){
-      if(_vibrator.hasVibrator()){
-          int duration = 50;
-          if (Build.VERSION.SDK_INT >= 26) {
-              _vibrator.vibrate(VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE));
-          } else {
-              _vibrator.vibrate(duration);
-          }
+    } else if (call.method.equals("success")) {
+      if (_vibrator.hasVibrator()) {
+        int duration = 50;
+        if (Build.VERSION.SDK_INT >= 26) {
+          _vibrator.vibrate(VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+          _vibrator.vibrate(duration);
+        }
       }
       result.success(null);
-    }
-    else if(call.method.equals("warning")){
-      if(_vibrator.hasVibrator()){
-          int duration = 250;
-          if (Build.VERSION.SDK_INT >= 26) {
-              _vibrator.vibrate(VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE));
-          } else {
-              _vibrator.vibrate(duration);
-          }
+    } else if (call.method.equals("warning")) {
+      if (_vibrator.hasVibrator()) {
+        int duration = 250;
+        if (Build.VERSION.SDK_INT >= 26) {
+          _vibrator.vibrate(VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+          _vibrator.vibrate(duration);
+        }
       }
       result.success(null);
-    }
-    else if(call.method.equals("error")){
-      if(_vibrator.hasVibrator()){
-          int duration = 500;
-          if (Build.VERSION.SDK_INT >= 26) {
-              _vibrator.vibrate(VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE));
-          } else {
-              _vibrator.vibrate(duration);
-          }
+    } else if (call.method.equals("error")) {
+      if (_vibrator.hasVibrator()) {
+        int duration = 500;
+        if (Build.VERSION.SDK_INT >= 26) {
+          _vibrator.vibrate(VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+          _vibrator.vibrate(duration);
+        }
       }
       result.success(null);
-    }
-    else if(call.method.equals("heavy")){
-      if(_vibrator.hasVibrator()){
-          int duration = 100;
-          if (Build.VERSION.SDK_INT >= 26) {
-              _vibrator.vibrate(VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE));
-          } else {
-              _vibrator.vibrate(duration);
-          }
+    } else if (call.method.equals("heavy")) {
+      if (_vibrator.hasVibrator()) {
+        int duration = 100;
+        if (Build.VERSION.SDK_INT >= 26) {
+          _vibrator.vibrate(VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+          _vibrator.vibrate(duration);
+        }
       }
       result.success(null);
-    }
-    else if(call.method.equals("medium")){
-      if(_vibrator.hasVibrator()){
-          int duration = 40;
-          if (Build.VERSION.SDK_INT >= 26) {
-              _vibrator.vibrate(VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE));
-          } else {
-              _vibrator.vibrate(duration);
-          }
+    } else if (call.method.equals("medium")) {
+      if (_vibrator.hasVibrator()) {
+        int duration = 40;
+        if (Build.VERSION.SDK_INT >= 26) {
+          _vibrator.vibrate(VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+          _vibrator.vibrate(duration);
+        }
       }
       result.success(null);
-    }
-    else if(call.method.equals("light")){
-      if(_vibrator.hasVibrator()){
-          int duration = 10;
-          if (Build.VERSION.SDK_INT >= 26) {
-              _vibrator.vibrate(VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE));
-          } else {
-              _vibrator.vibrate(duration);
-          }
+    } else if (call.method.equals("light")) {
+      if (_vibrator.hasVibrator()) {
+        int duration = 10;
+        if (Build.VERSION.SDK_INT >= 26) {
+          _vibrator.vibrate(VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+          _vibrator.vibrate(duration);
+        }
       }
       result.success(null);
-    }
-    else {
+    } else {
       result.notImplemented();
     }
   }
